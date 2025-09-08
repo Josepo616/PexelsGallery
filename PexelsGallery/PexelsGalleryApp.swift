6d63b34247727f98722ddc57931bef4099f27e5c
José Álvarez <josemejia61976751@gmail.com>
2025-09-08 12:06:04 -0600

    chore: initial commit

---
+++ PexelsGallery/PexelsGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsGalleryApp.swift
+//  PexelsGallery
+//
+//  Created by JoseAlvarez on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PexelsGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PexelsGalleryTests/PexelsGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsGalleryTests.swift
+//  PexelsGalleryTests
+//
+//  Created by JoseAlvarez on 9/8/25.
+//
+
+import Testing
+@testable import PexelsGallery
+
+struct PexelsGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
